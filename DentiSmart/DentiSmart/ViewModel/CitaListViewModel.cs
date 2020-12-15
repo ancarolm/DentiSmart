36d36c6728cff2aca01d72bad7634f23af8cdeca
marcoss2599 <75094366+marcoss2599@users.noreply.github.com>
2020-12-14 22:16:12 -0600

    Add files via upload

---
+++ DentiSmart/DentiSmart/ViewModel/CitaListViewModel.cs
@@ -0,0 +1,60 @@
+﻿using DentiSmart.Model;
+using DentiSmart.View;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace DentiSmart.ViewModel
+{
+    public class CitaListViewModel
+    {
+        public IList<Cita> Citas { get; set; }
+        public Command AddCitaCommand { get; set; }
+        private INavigation Navigation;
+        private Cita _currentCita;
+        public Command ItemTappedCommand { get; set; }
+        
+        public Cita CurrentCita
+        {
+            get { return _currentCita; }
+            set
+            {
+                _currentCita = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public CitaListViewModel(INavigation navigation)
+        {
+            CitaRepository repository = new CitaRepository();
+            Citas = repository.GetAll();
+            Navigation = navigation;
+            AddCitaCommand = new Command(async () => await NavigateToCitaView());
+            ItemTappedCommand = new Command(async () => await NavigateToEditCitaView());
+        }
+
+        public async Task NavigateToCitaView()
+        {
+            await Navigation.PushAsync(new Cita());
+        }
+
+        public async Task NavigateToEditCitaView()
+        {
+            await Navigation.PushAsync(new Cita(CurrentCita));
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+    }
+    
+}
---
+++ DentiSmart/DentiSmart/ViewModel/CitaViewModel.cs
@@ -0,0 +1,39 @@
+﻿using DentiSmart.Model;
+using DentiSmart.View;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+namespace DentiSmart.ViewModel
+{
+    public class CitaViewModel
+    {
+        public Command SaveCitaCommand { get; set; }
+        public Model.Cita NewCita { get; set; }
+        private INavigation Navigation;
+
+        public CitaViewModel(INavigation navigation)
+        {
+            NewCita = new Model.Cita();
+            SaveCitaCommand = new Command(async () => await SaveCita());
+            Navigation = navigation; ;
+
+        }
+
+        public CitaViewModel(INavigation navigation, Model.Cita cita)
+        {
+            NewCita = cita;
+            SaveCitaCommand = new Command(async () => await SaveCita());
+            Navigation = navigation;
+        }
+
+        public async Task SaveCita()
+        {
+            await App.Database.SaveCitaAsync(NewCita);
+            await Navigation.PopToRootAsync();
+        }
+    }
+}
