e2196112bd3e877dafb79a4392fef0e7036323f9
tonymarchena26 <39474827+tonymarchena26@users.noreply.github.com>
2020-12-15 02:12:47 -0600

    Add files via upload

---
+++ DentiSmart/DentiSmart/ViewModel/RegisterViewModel.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using GalaSoft.MvvmLight.Command;
+using Xamarin.Forms;
+using Prueba.Model;
+using Prueba.Views.AccessApp;
+
+namespace Prueba.ViewModel
+{
+    public class RegisterViewModel : BaseViewModel
+    {
+
+        #region Attributes
+        public string email;
+        public string password;
+        public string name;
+        public string age;
+
+        public bool isRunning;
+        public bool isVisible;
+        public bool isEnabled;
+        #endregion
+
+        #region Properties
+        public string EmailTxt
+        {
+            get { return this.email; }
+            set { SetValue(ref this.email, value); }
+        }
+
+        public string PasswordTxt
+        {
+            get { return this.password; }
+            set { SetValue(ref this.password, value); }
+        }
+
+        public string NameTxt
+        {
+            get { return this.name; }
+            set { SetValue(ref this.name, value); }
+        }
+
+        public string AgeTxt
+        {
+            get { return this.age; }
+            set { SetValue(ref this.age, value); }
+        }
+
+        public bool IsVisibleTxt
+        {
+            get { return this.isVisible; }
+            set { SetValue(ref this.isVisible, value); }
+        }
+
+        public bool IsEnabledTxt
+        {
+            get { return this.isEnabled; }
+            set { SetValue(ref this.isEnabled, value); }
+        }
+
+        public bool IsRunningTxt
+        {
+            get { return this.isRunning; }
+            set { SetValue(ref this.isRunning, value); }
+        }
+
+        #endregion
+
+        #region Commands
+        public ICommand RegisterCommand
+        {
+            get
+            {
+                return new RelayCommand(RegisterMethod);
+            }
+        }
+        #endregion
+
+        #region Methods
+        private async void RegisterMethod()
+        {
+            if (string.IsNullOrEmpty(this.email))
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error",
+                    "Debe agregar un correo.",
+                    "Aceptar");
+                return;
+            }
+
+            if (string.IsNullOrEmpty(this.password))
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error",
+                    "Debe agregar una contraseña.",
+                    "Aceptar");
+                return;
+            }
+
+            
+            if (string.IsNullOrEmpty(this.name))
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error",
+                    "Debe agregar su nombre.",
+                    "Aceptar");
+                return;
+            }
+            if (string.IsNullOrEmpty(this.age))
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error",
+                    "Debe agregar la edad.",
+                    "Aceptar");
+                return;
+            }
+
+            this.IsVisibleTxt = true;
+            this.IsRunningTxt = true;
+            this.IsEnabledTxt = false;
+
+
+            var user = new UserModel
+            {
+                EmailField = email,
+                PasswordField = password,
+                NamesField = name,
+                AgeField = age,
+                Creation_Date = DateTime.Now,
+            };
+
+            await App.Database.SaveUserModelAsync(user);
+
+            await Application.Current.MainPage.DisplayAlert("Cuenta registrada", "Bienvenido " + name.ToString() + " a DentiSmart", "Ok");
+
+            this.IsRunningTxt = false;
+            this.IsVisibleTxt = false;
+            this.IsEnabledTxt = true;
+
+            await Application.Current.MainPage.Navigation.PushAsync(new LoginPage());
+            
+        }
+        #endregion
+
+        #region Constructor
+        public RegisterViewModel()
+        {
+            IsEnabledTxt = true;
+        }
+        #endregion
+
+    }
+}
