a08ded651d118415bd8653f0dfd2b8d70ca96069
marcoss2599 <75094366+marcoss2599@users.noreply.github.com>
2020-12-14 22:15:34 -0600

    Add files via upload

---
+++ DentiSmart/DentiSmart/Model/Cita.cs
@@ -0,0 +1,55 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace DentiSmart.Model
+{
+    public class Cita : INotifyPropertyChanged
+    {
+        public string _date;
+        public string _time;
+        public string _action;
+
+        [PrimaryKey, AutoIncrement]
+        public int citaID { get; set; }
+
+        public string Date
+        {
+            get { return _date; }
+            set
+            {
+                _date = value;
+                OnPropertyChanged();
+            }
+        }
+        public string Time
+        {
+            get { return _time; }
+            set
+            {
+                _time = value;
+                OnPropertyChanged();
+            }
+        }
+        public string Action
+        {
+            get { return _action; }
+            set
+            {
+                _action = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+    
+}
---
+++ DentiSmart/DentiSmart/Model/CitaRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DentiSmart.Model
+{
+    public class CitaRepository
+    {
+        public IList<Cita> Citas{ get; set; }
+        public Cita cita
+        { get; set; }
+
+        public CitaRepository()
+        {
+            Task.Run(async () => Citas = (IList<Cita>)await App.Database.GetDoctorAsync()).Wait();
+        }
+
+        public IList<Cita> GetAll()
+        {
+            return Citas;
+        }
+    }
+}
