b59e43aa1a3a12964b8f86fbcbc968191b4fa8e5
tonymarchena26 <39474827+tonymarchena26@users.noreply.github.com>
2020-12-15 02:14:07 -0600

    Update BaseViewModel.cs

--- DentiSmart/DentiSmart/ViewModel/BaseViewModel.cs
+++ DentiSmart/DentiSmart/ViewModel/BaseViewModel.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
 
-namespace Prueba.ViewModel
+namespace DentiSmart.ViewModel
 {
     public class BaseViewModel : INotifyPropertyChanged
 
